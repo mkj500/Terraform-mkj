d55699aa01620e69be96afb5e40ded4b65143fbd
mkj <mkjosh500@gmail.com>
2026-01-29 16:22:55 +0100

    Add load balancer

--- level2/data.tf
+++ level2/data.tf
@@ -1,9 +1,9 @@
-data "terraform_remote_state" "level1"{
-    backend = "s3"
+data "terraform_remote_state" "level1" {
+  backend = "s3"
 
-    config = {
-        bucket = "mkj-cloudshell-s3bucket-31123"
-        key    = "level1.tfstate"
-        region = "us-east-1"
-    }
+  config = {
+    bucket = "mkj-cloudshell-s3bucket-31123"
+    key    = "level1.tfstate"
+    region = "us-east-1"
+  }
 }
--- level2/user-data.sh
+++ level2/user-data.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+set -eux
 yum update -y
-yum install -y nginx git
+amazon-linux-extras install -y nginx1
 systemctl enable nginx
 systemctl start nginx
-
+/*
 rm -rf /usr/share/nginx/html/*
 git clone https://github.com/gabrielecirulli/2048.git /usr/share/nginx/html
+*/
 
+cat <<EOF > /usr/share/nginx/html/index.html "Hello from instance ${INSTANCE_ID}"
+Private IP: ${PRIVATE_IP}
+EOF
 chown -R nginx:nginx /usr/share/nginx/html
 chmod -R 755 /usr/share/nginx/html
 systemctl restart nginx
