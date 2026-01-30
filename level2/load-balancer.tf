aa6c7570342d256df66acfa5e48a0c8c18aca6f8
mkj <mkjosh500@gmail.com>
2026-01-30 01:39:11 +0100

    Edit user data and load balancer

--- level2/load-balancer.tf
+++ level2/load-balancer.tf
@@ -45,7 +45,7 @@ resource "aws_lb_target_group" "main" {
   health_check {
     enabled             = true
     path                = "/"
-    port                = "traffic-port"
+    protocol            = "HTTP"
     healthy_threshold   = 5
     unhealthy_threshold = 2
     timeout             = 5
--- level2/user-data.sh
+++ level2/user-data.sh
@@ -1,16 +1,26 @@
+#!/bin/bash
 set -eux
 yum update -y
-amazon-linux-extras install -y nginx1
+amazon-linux-extras install nginx1 -y
 systemctl enable nginx
 systemctl start nginx
 /*
-rm -rf /usr/share/nginx/html/*
-git clone https://github.com/gabrielecirulli/2048.git /usr/share/nginx/html
+rm-rf /usr/sharenginx/html/
+git clone https://github.com/gabrielecirulli/2048.git /use/share/nginx/html
 */
-
-cat <<EOF > /usr/share/nginx/html/index.html "Hello from instance ${INSTANCE_ID}"
-Private IP: ${PRIVATE_IP}
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>EC2 Instance</title>
+</head>
+<body>
+  <h1>Hello from instance $INSTANCE_ID</h1>
+</body>
+</html>
 EOF
 chown -R nginx:nginx /usr/share/nginx/html
 chmod -R 755 /usr/share/nginx/html
 systemctl restart nginx
+
